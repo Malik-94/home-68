18843fb4299ad9a3a7f5e35b583395453c9102e5
Malik-94 <imenov-a@mail.ru>
2019-12-02 19:02:21 +0600

    home 68 1 этап сделал

--- source/main/urls.py
+++ source/main/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from webapp.views import api_add,api_subtract,api_multiply,api_divide
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('add/', api_add, name='add_api'),
+    path('subtract/', api_subtract, name='subtract_api'),
+    path('multiply/', api_multiply, name='multiply_api'),
+    path('divide/', api_divide, name='divide_api')
+
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+import json
+from datetime import datetime
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+@csrf_exempt
+def api_add(request, *args, **kwargs):
+    result = None
+    if request.body:
+        request_data = json.loads(request.body)
+        a = request_data.get('A')
+        b = request_data.get('B')
+        try:
+            a = int(a)
+            b = int(b)
+            result = a + b
+            # data = {
+            #     'answer': result
+            # }
+            return JsonResponse({
+                'result' : result
+            })
+        except ValueError:
+            response = JsonResponse({'error': 'Bad request'})
+            response.status_code = 400
+            return response
+
+
+
+@csrf_exempt
+def api_subtract(request, *args, **kwargs):
+    result = None
+    if request.body:
+        request_data = json.loads(request.body)
+        a = request_data["A"]
+        b = request_data["B"]
+        result = a - b
+    data = {
+        'answer': result
+    }
+    return JsonResponse(data)
+
+
+@csrf_exempt
+def api_multiply(request, *args, **kwargs):
+    result = None
+    if request.body:
+        request_data = json.loads(request.body)
+        a = request_data["A"]
+        b = request_data["B"]
+        result = a * b
+    data = {
+        'answer': result
+    }
+    return JsonResponse(data)
+
+
+@csrf_exempt
+def api_divide(request, *args, **kwargs):
+    result = None
+    if request.body:
+        request_data = json.loads(request.body)
+        a = request_data["A"]
+        b = request_data["B"]
+        result = a / b
+    data = {
+        'answer': result
+    }
+    return JsonResponse(data)
